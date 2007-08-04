9ac11afe9fca0531a2750fe4dfbf5922b5f438c7
Darren Hart <darren@dvhart.com>
2007-08-04 04:55:10 +0000

    Added updated GPL headers.
    Signed-off-by: Darren Hart <darren@dvhart.com>

--- gtd_tree.py
+++ gtd_tree.py
@@ -1,3 +1,25 @@
+#    Filename: gtd_tree.py
+#      Author: Darren Hart <darren@dvhart.com>
+# Description: gtd classes (task, contexts, etc.)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Copyright (C) Darren Hart, 2007
+#
+# 2007-Jun-30:	Initial version by Darren Hart <darren@dvhart.com>
+
 import pickle
 
 class base:
--- pydo.py
+++ pydo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #    Filename: pydo.py
-#      Author: Darren Hart <dvhltc@us.ibm.com>
-# Description: 
+#      Author: Darren Hart <darren@dvhart.com>
+# Description: Pydo application class and program initialization code
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-# Copyright (C) IBM Corporation, 2006
+# Copyright (C) Darren Hart, 2007
 #
 # 2007-Jun-30:	Initial version by Darren Hart <darren@dvhart.com>
 
