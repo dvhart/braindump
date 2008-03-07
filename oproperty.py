4c2b7a5db0e5c615122709e3cab5519bb8866598
Darren Hart <darren@dvhart.com>
2008-03-07 07:52:20 +0000

    Derived property support (OProperty)
    This derivable version of properties checks for the named function in the
    actual class description, and uses that if it exists, otherwise it defaults
    to the statically bound property function.
    
    Original source from:
    http://infinitesque.net/articles/2005/enhancing%20Python's%20property.xhtml
    
    Signed-off-by: Darren Hart <darren@dvhart.com>

---
+++ oproperty.py
@@ -0,0 +1,41 @@
+#    Filename: oproperty.py
+#      Source: http://infinitesque.net/articles/2005/enhancing%20Python's%20property.xhtml
+# Description: This provides an alternative to Python's built-in property
+#              function which allows the user to override, in derived classes,
+#              the methods used by this function.
+# 2005-Nov-02: Revision 0.1
+
+class OProperty(object):
+    """Based on the emulation of PyProperty_Type() in Objects/descrobject.c"""
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError, "unreadable attribute"
+        if self.fget.__name__ == '<lambda>' or not self.fget.__name__:
+            return self.fget(obj)
+        else:
+            return getattr(obj, self.fget.__name__)()
+
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError, "can't set attribute"
+        if self.fset.__name__ == '<lambda>' or not self.fset.__name__:
+            self.fset(obj, value)
+        else:
+            getattr(obj, self.fset.__name__)(value)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError, "can't delete attribute"
+        if self.fdel.__name__ == '<lambda>' or not self.fdel.__name__:
+            self.fdel(obj)
+        else:
+            getattr(obj, self.fdel.__name__)()
