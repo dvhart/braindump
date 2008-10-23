af016b3a6f6a40eaf71eeca37bb971b1a0e402fd
Darren Hart <darren@dvhart.com>
2008-10-23 14:18:35 +0000

    Separate the date select popup into its own class
    Signed-off-by: Darren Hart <darren@dvhart.com>

--- gui/date_select.py
+++ gui/date_select.py
@@ -1,18 +1,158 @@
 #    Filename: date_select.py
-# Description: a more functional date selector
+# Description: A more functional date selector.
 #
-# 2008-Oct-02: Initial version
+# Copyright (C) Darren Hart, 2008
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Author(s): Darren Hart <darren@dvhart.com>
+#
+# Acknowledgements:
+# o Johan Dahlin for the kiwi dateentry widget, and the insight it provided
+#   into the quirks of the gtk *grab* routines.
 
 from gobject import *
 import gtk
-from gtk import gdk
+from gtk import gdk, keysyms
 from gtk.gdk import keyval_from_name
 from friendly_date import *
 from logging import debug, info, warning, error, critical
 
 class _DateSelectPopup(gtk.Window):
+    """A friendly date selection popup window"""
+
+    __gsignals__ = {'date-selected' : (SIGNAL_RUN_FIRST, TYPE_NONE, (TYPE_PYOBJECT,)),
+                    'popdown' : (SIGNAL_RUN_FIRST, TYPE_NONE, ())}
+
     def __init__(self):
-        gtk.Window.__init__(self)
+        gtk.Window.__init__(self, gtk.WINDOW_POPUP)
+
+        frame = gtk.Frame()
+        frame.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        self.add(frame)
+        frame.show()
+
+        vbox = gtk.VBox()
+        frame.add(vbox)
+        vbox.show()
+
+        self._calendar = gtk.Calendar()
+        vbox.pack_start(self._calendar)
+        self._calendar.show()
+
+        btn_box = gtk.HButtonBox()
+        btn_box.set_border_width(6)
+        vbox.pack_start(btn_box)
+        btn_box.show()
+
+        for label in ["None", "Today", "Tomorrow"]:
+            button = gtk.Button(label)
+            button.connect("clicked", self._on_date_button_clicked, label)
+            btn_box.pack_start(button)
+            button.show()
+
+        self.connect("button-press-event", self._on_button_press)
+        self.connect("key-press-event", self._on_key_press)
+        self._calendar.connect("day-selected", self._on_day_selected)
+
+    # private interface
+    def _grab_window(self):
+        activate_time = 0L
+        if gdk.pointer_grab(self.window, True,
+                         gdk.BUTTON_PRESS_MASK | 
+                         gdk.BUTTON_RELEASE_MASK |
+                         gdk.POINTER_MOTION_MASK,
+                         None, None, activate_time) == 0:
+            if gdk.keyboard_grab(self.window, True, activate_time) == 0:
+                return True
+            else:
+                self.window.get_display().pointer_ungrab(activate_time);
+                return False
+        return False
+
+    def _on_button_press(self, window, event):
+        hide = False
+
+        # check if we clicked outside the popup
+        w, h = self.get_size()
+        x = event.x
+        y = event.y
+        if x < 0 or x > w or y < 0 or y > h:
+            hide = True
+
+        # check if we clicked on another widget in our app
+        toplevel = event.window.get_toplevel()
+        parent = self.get_parent_window()
+        if toplevel != parent:
+            hide = True
+
+        if hide:
+            self.popdown()
+
+    def _on_key_press(self, popup, event):
+        """
+        Mimics Combobox behavior
+
+        Escape or Alt+Up: Close
+        """
+
+        keyval = event.keyval
+        state = event.state & gtk.accelerator_get_default_mod_mask()
+        if (keyval == keysyms.Escape or
+            ((keyval == keysyms.Up or keyval == keysyms.KP_Up) and
+             state == gdk.MOD1_MASK)):
+            self.popdown()
+            return True
+
+    def _on_day_selected(self, cal):
+        if self.get_property("visible"):
+            self.popdown()
+        year, month, day = cal.get_date()
+        datetime_val = datetime(year, month + 1, day)
+        datetime_val = datetime_ceiling(datetime_val)
+        self.emit("date-selected", datetime_val)
+
+    def _on_date_button_clicked(self, btn, friendly_str):
+        self.popdown()
+        datetime_val = friendly_to_datetime(friendly_str)
+        self.emit("date-selected", datetime_val)
+
+    # public interface
+    def popdown(self):
+        self.grab_remove()
+        self.hide_all()
+        self.emit("popdown")
+
+    def popup(self, widget):
+        self.realize()
+        x, y = widget.window.get_origin()
+        x += widget.get_allocation().x
+        y += widget.get_allocation().y + widget.get_allocation().height
+        self.move(x, y)
+        self.show_all()
+
+        if not self._grab_window():
+            error("failed to grab window")
+
+        self.grab_add()
+
+    def set_date(self, datetime_val):
+        if datetime_val:
+            self._calendar.select_month(datetime_val.month-1, datetime_val.year)
+            self._calendar.select_day(datetime_val.day)
+
 
 class DateSelect(gtk.ToggleButton):
     """A more functional date selector"""
@@ -34,118 +174,20 @@ class DateSelect(gtk.ToggleButton):
         self.add(hbox)
         self.show_all()
 
-        # build the popup widgets
-        self._popup = gtk.Window(gtk.WINDOW_POPUP)
-        frame = gtk.Frame()
-        frame.set_shadow_type(gtk.SHADOW_ETCHED_IN)
-        vbox = gtk.VBox()
-        self.date_calendar = gtk.Calendar()
-        date_none = gtk.Button("None")
-        date_today = gtk.Button("Today")
-        date_tomorrow = gtk.Button("Tomorrow")
-        date_this_friday = gtk.Button("This Friday")
-        date_next_week = gtk.Button("Next Week")
+        self._popup = _DateSelectPopup()
 
-        # assemble the popup
-        frame.add(vbox)
-        vbox.pack_start(self.date_calendar)
-        vbox.pack_start(date_none)
-        vbox.pack_start(date_today)
-        vbox.pack_start(date_tomorrow)
-        vbox.pack_start(date_this_friday)
-        vbox.pack_start(date_next_week)
-        self._popup.add(frame)
-
-        # connect the signals
         self.connect("toggled", self._on_button_toggled)
-        self._popup.connect("button-press-event", self._on_popup_button_press)
-        self.date_calendar.connect("day-selected", self._on_day_selected)
-        date_none.connect("clicked", self._on_date_button_clicked, "None")
-        date_today.connect("clicked", self._on_date_button_clicked, "Today")
-        date_tomorrow.connect("clicked", self._on_date_button_clicked, "Tomorrow")
-        date_this_friday.connect("clicked", self._on_date_button_clicked, "This Friday")
-        date_next_week.connect("clicked", self._on_date_button_clicked, "Next Week")
+        self._popup.connect("date-selected", lambda w,d: self.set_date(d))
+        self._popup.connect("popdown", lambda w: self.set_active(False))
 
         self._date = None
 
-    def popdown(self):
-        self._popup.grab_remove()
-        self._popup.hide_all()
-        self.set_active(False)
-
-    def _popup_grab_window(self):
-        activate_time = 0L
-        if gdk.pointer_grab(self._popup.window, True,
-                         gdk.BUTTON_PRESS_MASK | 
-                         gdk.BUTTON_RELEASE_MASK |
-                         gdk.POINTER_MOTION_MASK,
-                         None, None, activate_time) == 0:
-            if gdk.keyboard_grab(self._popup.window, True, activate_time) == 0:
-                return True
-            else:
-                self._popup.window.get_display().pointer_ungrab(activate_time);
-                return False
-        return False
-
-    def popup(self):
-        x, y = self.window.get_origin()
-        x += self.get_allocation().x
-        y += self.get_allocation().y + self.get_allocation().height
-        self._popup.move(x, y)
-        self._popup.show_all()
-
-        if not self._popup_grab_window():
-            self._popup.hide_all()
-
-        self._popup.grab_add()
-
-
-    def _on_popup_button_press(self, window, event):
-        hide = False
-
-        # check if we clicked outside the popup
-        w, h = self._popup.get_size()
-        x = event.x
-        y = event.y
-        if x < 0 or x > w or y < 0 or y > h:
-            hide = True
-
-        # check if we clicked on another widget in our app
-        toplevel = event.window.get_toplevel()
-        parent = self.date_calendar.get_parent_window()
-        if toplevel != parent:
-            hide = True
-
-        if hide:
-            self.popdown()
-
-    # FIXME: close the popup if esc is pressed
-    def _on_popup_key_press(self, popup, event):
-        pass
-
-    # FIXME: find a way to popup on button pressed, not just toggled
     def _on_button_toggled(self, widget):
         if self.get_active():
-            self.popup()
-        else:
-            self._hide_popup()
-
-    def _on_day_selected(self, cal):
-        self._popup.hide()
-        self.set_active(False)
-        year, month, day = cal.get_date()
-        datetime_val = datetime(year, month + 1, day)
-        datetime_val = datetime_ceiling(datetime_val)
-        self.set_date(datetime_val)
-
-    def _on_date_button_clicked(self, btn, friendly_str):
-        self._popup.hide()
-        self.set_active(False)
-        datetime_val = friendly_to_datetime(friendly_str)
-        self.set_date(datetime_val)
+            self._popup.set_date(self._date)
+            self._popup.popup(self)
 
     # public interface
-    # FIXME: add an api to manage the date shortcut buttons
     def get_date(self):
         """Return the selected date"""
         return self._date
